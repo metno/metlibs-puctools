6ddfa768f7295e88564dc4f3f076a1fa94cf172e
Dag Erling Smørgrav <dages@met.no>
2010-01-22 14:56:27 +0000

    Import glob() and strlcpy() (needed by glob()) from OpenSSH.

---
+++ strlcpy.h
@@ -0,0 +1,11 @@
+/* $Id$ */
+#ifndef STRLCPY_H_INCLUDED
+#define STRLCPY_H_INCLUDED
+#ifdef __cplusplus
+extern "C" {
+#endif
+size_t strlcpy(char *, const char *, size_t);
+#ifdef __cplusplus
+}
+#endif
+#endif
